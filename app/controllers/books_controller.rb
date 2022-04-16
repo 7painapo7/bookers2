7ca75cd23aa08a2a224f14b099d32fc4b4d7a54c
“7painapo7” <53612675+7painapo7@users.noreply.github.com>
2022-04-16 10:50:58 +0000

    [Finish]Post Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,42 @@
 class BooksController < ApplicationController
-  def show
+
+  # 本一蘭と投稿機能を実装
+  def index
+    @book = Book.new
+    @books = Book.all
+
+    @user = current_user
   end
 
-  def index
+  def show
+    @book = Book.new
+  	@book_show = Book.find(params[:id])
+    @user = User.find(@book_show.user_id)
+
+    # @post_comment = PostComment.new
   end
 
   def edit
   end
+
+  def create
+    @user = current_user
+  	@book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @users = User.all
+    if
+      @book.save
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(@book.id)
+      # redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+	def book_params
+	  params.require(:book).permit(:title, :body)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
-  get 'books/show'
-  get 'books/index'
-  get 'books/edit'
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+  resources :books
+  resources :users
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
